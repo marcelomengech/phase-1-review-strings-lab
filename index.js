3c4cf1861d0af6dd78692c15e2da80f5d462bfb8
marcelomengech <marcelomengech@gmail.com>
2024-11-27 13:12:55 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser= 'Marcellus Mengech';
+
+const welcomeMessage= 'Welcome to Flatbook, Marcellus Mengech!';
+
+const excitedWelcomeMessage= 'WELCOME TO FLATBOOK, MARCELLUS MENGECH!';
+const shortGreeting= 'Welcome, M!';
+
